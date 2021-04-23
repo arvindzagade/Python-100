b2b82659ce7108340b45bdd98eeb257ad7debe93
arvindzagade <arvindzagade@gmail.com>
2021-04-23 14:47:20 +0530

    Update Get_filetime.py

--- Get_filetime.py
+++ Get_filetime.py
@@ -12,3 +12,19 @@ def is_file_older_than (file, delta):
 is_file_older_than(filename, timedelta(seconds=10))
 
 #is_file_older_than(filename, timedelta(days=10))
+
+
+
+
+
+# print("Last modified: %s" % time.ctime(os.path.getmtime(file)))
+    # print("current time:%s" % datetime.utcnow())
+    # diff = datetime.utcnow() - datetime.utcfromtimestamp(os.path.getmtime(file))
+    # print(diff)
+    #filetime = "Last modified: %s" % time.ctime(os.path.getmtime(file))
+    # now = datetime.now()
+    # now = now.strftime('%Y-%m-%d %H:%M:%S')
+    # print(now)
+    # FMT = '%Y-%m-%d %H:%M:%S'
+    # time_diff = datetime.strptime(now,FMT) - datetime.strptime(file,FMT)
+    # print(time_diff)
